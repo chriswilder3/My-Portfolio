b39836100614767273f466e085078883a6d89a6c
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-19 22:15:18 +0530

    added few components

---
+++ src/components/Home/Heros.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Heros() {
+  return (
+    <div>
+        <h2 className='text-3xl font-semibold'>
+            Hey, I'm
+        </h2> 
+        <h1 className='text-5xl font-bold'>
+            Sachin Shivaji Doddamani
+        </h1>
+    </div>
+  )
+}
+
+export default Heros
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heros from './Heros';
+
+function Home() {
+  return (
+    <>
+        <Heros />
+    </>
+  )
+}
+
+export default Home
